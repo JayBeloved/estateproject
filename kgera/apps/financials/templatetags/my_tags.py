e3468fec09cd8b101cea7ac529524532dd03126c
John J. Lawal <jophilip79@gmail.com>
2022-04-16 20:22:43 +0100

    Fixed Template syntax error

--- kgera/apps/financials/templatetags/my_tags.py
+++ kgera/apps/financials/templatetags/my_tags.py
@@ -4,8 +4,8 @@ from ..models import Residents
 register = template.Library()
 
 
-@register.filter(name='get_id', is_safe=True)
-def get_id(resident_code):
+@register.filter(name='get_res_id', is_safe=True)
+def get_res_id(resident_code):
     res_id = Residents.objects.get(resident_code=resident_code).id
 
     return res_id
