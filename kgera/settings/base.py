0271123d6f9638853fa24c68828137de79779e8a
John J. Lawal <jophilip79@gmail.com>
2022-06-04 13:17:46 +0100

    update login details

--- kgera/settings/base.py
+++ kgera/settings/base.py
@@ -94,7 +94,7 @@ DATABASES = {
     'default': {
     'ENGINE': 'mysql.connector.django',
     'NAME': 'kgeraorg_kgera', 
-    'USER':'kgeraorg_kgeraorg', 
+    'USER':'kgeraorg_kgera', 
     'PASSWORD':'kabusa2021_',  
     'HOST':'kgera.org.ng', 
     'PORT':'3306',
