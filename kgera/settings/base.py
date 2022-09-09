0cc1a3a6fbb3789bd2d0f336980728672d7fc4c7
John J. Lawal <jophilip79@gmail.com>
2022-09-09 12:58:03 +0100

    Changes TZ setting

--- kgera/settings/base.py
+++ kgera/settings/base.py
@@ -121,13 +121,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = config("LANGUAGE_CODE", default="en-us")
 
-TIME_ZONE = config("TIME_ZONE", default="UTC +1")
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 LOCALE_PATHS = [BASE_DIR / "locale"]
 
